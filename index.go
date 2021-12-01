b20368f0e9ae3236a9bdc269b5dd3a1ea312bfa0
k-samuel <kirill.a.egorov@gmail.com>
2021-12-01 22:26:08 +0300

    Github actions test

--- index.go
+++ index.go
@@ -6,12 +6,32 @@ import (
 	"sync"
 )
 
+/*
+ *  Data Structure
+ *   Index {
+ *   	ids : []  -  record id list
+ *      fields: [
+ *           'field1' => [] Field{
+ *               values: => [
+ *               	'val1' => Value{
+ *                        ids: => [1,2,3,4]
+ *                   },
+ *                   'val2' => ...
+ *               ]
+ *	         },
+ *           'field2 => ...
+*      ]
+ *   }
+*/
+
+// Index - top level structure for facet data
 type Index struct {
 	fields map[string]*Field
 	ids    []int64
 	mu     sync.Mutex
 }
 
+// NewIndex  - Index constructor
 func NewIndex() *Index {
 	var index Index
 	index.fields = make(map[string]*Field)
@@ -19,8 +39,16 @@ func NewIndex() *Index {
 	return &index
 }
 
+// GetData get fields data
 func (index *Index) GetData() map[string]*Field {
 	return index.fields
+}
+
+// GetAllRecordId get all record id stored in index
+func (index *Index) GetAllRecordId() []int64 {
+	data := make([]int64, len(index.ids))
+	copy(data, index.ids)
+	return data
 }
 
 func (index *Index) Add(id int64, record map[string]interface{}) {
@@ -57,13 +85,6 @@ func (index *Index) GetFieldData(fieldName string) (val *Field, ok bool) {
 		return index.fields[fieldName], true
 	}
 	return val, false
-}
-
-// GetAllRecordId get all record id stored in index
-func (index *Index) GetAllRecordId() []int64 {
-	data := make([]int64, len(index.ids))
-	copy(data, index.ids)
-	return data
 }
 
 func (index *Index) addValue(id int64, key string, val interface{}) {
@@ -114,6 +135,7 @@ func (index *Index) addValue(id int64, key string, val interface{}) {
 	value.addId(id)
 }
 
+// getValueString - convert value to string
 func getValueString(val interface{}) string {
 
 	if s, ok := val.(bool); ok {
@@ -144,11 +166,13 @@ func getValueString(val interface{}) string {
 	panic("undefined value type")
 }
 
+// Field - struct to store value list for index field
 type Field struct {
 	values map[string]*Value
 	mu     sync.Mutex
 }
 
+// HasValues - check if field has any value
 func (field *Field) HasValues() bool {
 	if len(field.values) > 0 {
 		return true
@@ -168,15 +192,18 @@ func (field *Field) createValue(name string) *Value {
 	return field.values[name]
 }
 
+// GetValue get field value by value string identifier
 func (field *Field) GetValue(name string) *Value {
 	return field.values[name]
 }
 
+// Value - list of record id for value
 type Value struct {
 	ids map[int64]struct{}
 	mu  sync.Mutex
 }
 
+// addId - add record id into value struct
 func (value *Value) addId(id int64) {
 	value.mu.Lock()
 	value.ids[id] = struct{}{}
