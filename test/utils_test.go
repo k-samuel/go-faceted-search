22335dfb7d94882160667d2a23a89cf860cd381b
k-samuel <kirill.a.egorov@gmail.com>
2022-01-16 05:07:13 +0300

    Fix intersect count

--- test/utils_test.go
+++ test/utils_test.go
@@ -6,23 +6,32 @@ import (
 	"testing"
 )
 
-func TestIntersectSortedInt(t *testing.T) {
-	src := []int64{1, 2}
-	cmp := []int64{1, 2}
+type testCaseData struct {
+	src []int64
+	cmp []int64
+	exp []int64
+}
 
-	res := utils.IntersectSortedInt(src, cmp)
-	exp := []int64{1, 2}
-	if !reflect.DeepEqual(exp, res) {
-		t.Errorf("results not match\nGot:\n%v\nExpected:\n%v", res, exp)
+func intersectData() []testCaseData {
+	return []testCaseData{
+		{src: []int64{1, 2}, cmp: []int64{1, 2}, exp: []int64{1, 2}},
+		{src: []int64{10, 21, 123, 124}, cmp: []int64{1, 2, 22, 123, 127}, exp: []int64{123}},
+		{src: []int64{1}, cmp: []int64{2, 3, 6}, exp: []int64{}},
+		{src: []int64{1, 7, 8, 9}, cmp: []int64{2, 7}, exp: []int64{7}},
+		{src: []int64{1, 2, 3, 4, 6}, cmp: []int64{3, 4, 5, 6}, exp: []int64{3, 4, 6}},
 	}
+}
 
-	src = []int64{1, 2, 7}
-	cmp = []int64{1, 8}
+func TestIntersectSortedInt(t *testing.T) {
 
-	res = utils.IntersectSortedInt(src, cmp)
-	exp = []int64{1}
-	if !reflect.DeepEqual(exp, res) {
-		t.Errorf("results not match\nGot:\n%v\nExpected:\n%v", res, exp)
+	data := intersectData()
+	var res []int64
+
+	for _, v := range data {
+		res = utils.IntersectSortedInt(v.src, v.cmp)
+		if !reflect.DeepEqual(v.exp, res) {
+			t.Errorf("results not match\nGot:\n%v\nExpected:\n%v", res, v.exp)
+		}
 	}
 }
 
